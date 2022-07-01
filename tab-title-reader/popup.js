ee2643729b0ba079a0ac8ff8f45b06d6cfe6b472
calisthenicsGuy <radevradoslav12@gmail.com>
2022-07-01 12:22:36 +0300

    getting url of current tab

--- tab-title-reader/popup.js
+++ tab-title-reader/popup.js
@@ -1,6 +1,16 @@
 let tabTitle = document.getElementById('tabTitle');
 tabTitle.innerHTML = document.title;
 
+let btnEl = document.querySelector('.get-curr-url-btn');
+
+btnEl.addEventListener('click', (e) => {
+  chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let object = tabs[0];
+    let url = object.url;
+    console.log(url);
+  })
+})
+
 async function getCurrentTabTitle() {
     let queryOptions = { active: true, lastFocusedWindow: true };
     // `tab` will either be a `tabs.Tab` instance or `undefined`.
